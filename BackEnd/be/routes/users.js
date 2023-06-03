bd115ca1d7b42e5918ebd651199fb3a09c3a7a28
AlexAgronov <98950218+AlexAgronov@users.noreply.github.com>
2023-06-03 17:14:37 +0300

    3.6.23

--- BackEnd/be/routes/users.js
+++ BackEnd/be/routes/users.js
@@ -1,10 +1,24 @@
 import express from "express";
 import { addTask } from "../controllers/task.js";
-import { deleteUser, updateUser, subjectLevel } from "../controllers/user.js";
+import {
+  deleteUser,
+  updateUser,
+  /** subjectLevel,*/
+  getStudentClasses,
+  getTeacherSubjects,
+  getTeacherClasses,
+  getTeachersStudent,
+  getCerfiticationForStud,
+} from "../controllers/user.js";
 
 const router = express.Router();
 router.delete("/delete", deleteUser);
 router.post("/updateuser", updateUser)
-router.get("/subjectlevel", subjectLevel);
+//router.get("/subjectlevel", subjectLevel);
+router.get("/studentcsrtif", getCerfiticationForStud);
+router.get("/studentclasses", getStudentClasses);
+router.get("/teachersubjects", getTeacherSubjects);
+router.get("/teacherclasses", getTeacherClasses);
+router.get("/teacherstudent", getTeachersStudent);
 
 export default router;
