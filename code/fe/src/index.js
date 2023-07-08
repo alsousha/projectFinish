13cbb44f338625f421a04ce692c316e089609068
alsubogd <alsousha@mail.ru>
2023-07-08 23:08:44 +0300

    add authors

--- code/fe/src/index.js
+++ code/fe/src/index.js
@@ -1,3 +1,6 @@
+//authors:
+//Bogdanova Alsu 333988939
+//Agronov Alex 321816092
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
